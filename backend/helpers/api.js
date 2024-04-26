6d10adff9da92fec7808c6e46363a8c5058a990c
Spencer Lin <spen7148@gmail.com>
2024-04-25 20:59:36 -0400

    changing port

--- backend/helpers/api.js
+++ backend/helpers/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =  "http://localhost:3001";
+const BASE_URL = process.env.PORT === "3001" ? "http://localhost:3001" : "http://localhost:10000";
 
 /** API Class.
  *
